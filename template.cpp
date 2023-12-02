5aefb9e6a7850e10c57f557fd12d335d8491393d
Arturo Hernández <arturoaureliohr@gmail.com>
2023-12-02 14:52:48 -0400

    Add template script

---
+++ template.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define fast_io() ios_base::sync_with_stdio(false); cin.tie(NULL)
+#define sz(x) (int)x.size()
+#define all(x) begin(x), end(x)
+#define MOD 1000000007
+typedef long long ll;
+typedef double db;
+
+/*
+ * <Problem title here> 
+ * <Problem Link here>
+*/
+
+using namespace std;
+
+int main() {
+	fast_io();
+}
